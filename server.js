04ab1cd09cb50f179391224eea510b117ca42bc0
Shubham Selakoti <86091766+shubhamselakoti@users.noreply.github.com>
2025-01-27 15:30:30 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -38,42 +38,51 @@ const SpeakXque = new mongoose.model("SpeakXQue", speakXqueSchema);
 
 
 app.get('/get', async (req, res) => {
-    try {
-      const page = parseInt(req.query.page) || 1;
-      const limit = parseInt(req.query.limit) || 20;
-      const skip = (page - 1) * limit;
-      const searchTerm = req.query.search || '';
-  
-      const searchQuery = searchTerm
-        ? { title: { $regex: new RegExp(searchTerm, 'i') } }
-        : {};
-  
-      const data = await SpeakXque.find(searchQuery)
-        .skip(skip)
-        .limit(limit);
-  
-      const totalDocuments = await SpeakXque.countDocuments(searchQuery);
-  
-      const totalPages = Math.ceil(totalDocuments / limit);
-  
-      res.status(200).json({
-        data,
-        totalPages,
-        currentPage: page,
-        totalDocuments,
-        limit,
-      });
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      res.status(500).json({ error: 'Failed to fetch data' });
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 20;
+    const skip = (page - 1) * limit;
+    const searchTerm = req.query.search || '';
+    const type = req.query.type || 'ALL';
+
+    let subType = '';
+    if (type.includes('ANAGRAM')) {
+      subType = type.slice(8);
     }
-  });
+
+    const searchQuery = {
+      ...(searchTerm && { title: { $regex: new RegExp(searchTerm, 'i') } }),
+      ...(type !== 'ALL' && { type: type.includes('ANAGRAM') ? 'ANAGRAM' : type }),
+      ...(type.includes('ANAGRAM') && subType && { anagramType: subType }),
+    };
+
+    const data = await SpeakXque.find(searchQuery)
+      .skip(skip)
+      .limit(limit);
+
+    const totalDocuments = await SpeakXque.countDocuments(searchQuery);
+
+    const totalPages = Math.ceil(totalDocuments / limit);
+
+    res.status(200).json({
+      data,
+      totalPages,
+      currentPage: page,
+      totalDocuments,
+      limit,
+    });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Failed to fetch data' });
+  }
+});
+
   
   
   
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 3001;
+  port = 5000;
 }
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
